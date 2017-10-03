0533422c4b5899eb17bf236d52ac3417dd9e3024
Adilet Maratov <adilet@cloudkite.com>
2017-10-03 23:08:02 +0600

    buggy solution for now

--- arrays/7_max_subarray_equal_number_of_0_and_1.py
+++ arrays/7_max_subarray_equal_number_of_0_and_1.py
@@ -1,14 +1,45 @@
 def linear_solution(A):
-    results = [0, 0]
+    results = [0, -1]
     sum_counter = 0
     A = [1 if x else -1 for x in A]
-    sums = {}
+    sums = {0: 0}
     for counter, value in enumerate(A):
         sum_counter += value
-        if not sums.get(sum_counter):
+        if sum_counter not in sums:
             sums[sum_counter] = counter
         else:
             if results[1]-results[0] < counter - sums[sum_counter]:
                 results[0] = sums[sum_counter]
-                results[1] = counter
+                results[1] = counter+1
     return [1 if x > 0 else 0 for x in A[results[0]:results[1]]]
+
+
+def naive_solution(A):
+    l, r = 0, -1
+    for i in range(len(A)):
+        zc = 0
+        oc = 0
+        for j in range(i, len(A)):
+            if A[j] == 1:
+                oc += 1
+            else:
+                zc += 1
+            if oc == zc and j - i > r - l:
+                l, r = i, j
+    return A[l:r+1]
+
+
+# print(linear_solution([0, 1, 0]))
+import random
+n = 3
+for _ in range(1000):
+    A = [random.randint(0, 1) for __ in range(n)]
+    naive = naive_solution(A)
+    linear = linear_solution(A)
+    try:
+        assert naive == linear
+    except AssertionError:
+        print(A)
+        print(naive)
+        print(linear)
+        break
