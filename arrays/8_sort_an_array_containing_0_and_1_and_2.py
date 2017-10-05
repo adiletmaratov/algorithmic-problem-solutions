85a51507fe4b3d33a2305b464a610919005181c1
Adilet Maratov <adilet@cloudkite.com>
2017-10-05 23:24:41 +0600

    sort an array containing 0, 1 and 2. Kinda Dutch national flag problem. Using partitioning

---
+++ arrays/8_sort_an_array_containing_0_and_1_and_2.py
@@ -0,0 +1,29 @@
+def linear_solution(A):
+    i, j, r, p = 0, 0, len(A) - 1, 1
+    while r >= j:
+        if A[j] < p:
+            A[j], A[i] = A[i], A[j]
+            i, j = i + 1, j + 1
+        elif A[j] > p:
+            A[j], A[r] = A[r], A[j]
+            r -= 1
+        else:
+            j += 1
+    return A
+
+
+#  stress test
+import random
+
+n = 100
+for _ in range(1000):
+    A = [random.randint(0, 2) for __ in range(n)]
+    python_sort = sorted(A)
+    linear = linear_solution(A)
+    try:
+        assert python_sort == linear
+    except AssertionError:
+        print(A)
+        print(python_sort)
+        print(linear)
+        break
